65a52a869cbcd35d8e874a5899f32683eaae648c
Leandro <teoferreira@gmail.com>
2024-05-03 10:24:01 -0300

    Avaliação PGATS - Programação para Automação- Melhorias

--- src/compras/compras.js
+++ src/compras/compras.js
@@ -1,13 +1,11 @@
-const assert = require('assert');
-
 const calcularTotal = (ferramentas, comprar) => {
     if (ferramentas.length === 0 || comprar.length === 0) {
         throw new Error("Ambas as listas precisam ter ao menos um item.");
     }  
     
     let total = 0;
+    let ferramentasEncontradas = [];
     if (ferramentas,comprar) {
-        let ferramentasEncontradas = [];
         for (let i = 0; i < comprar.length; i++) {
             for (let j = 0; j < ferramentas.length; j++) {
                 if (ferramentas[j].nome === comprar[i]) {
@@ -18,9 +16,17 @@ const calcularTotal = (ferramentas, comprar) => {
     }
     if (ferramentasEncontradas.length === 0 ) {
         throw new Error("Nenhuma ferramenta desejada encontrada.");
-    }   
+    } 
 
-    const resultado = `O valor a pagar pelas ferramentas (UFT, TOSCA) é R$ ${total.toFixed(2)}`;
+    let nomesFerramentas = '';
+    for (let i = 0; i < ferramentasEncontradas.length; i++) {
+        nomesFerramentas += ferramentasEncontradas[i].nome;
+        if (i < ferramentasEncontradas.length - 1) {
+            nomesFerramentas += ', ';
+        }
+    }
+
+    const resultado = `O valor a pagar pelas ferramentas (${nomesFerramentas}) é R$ ${total.toFixed(2)}`;
     return resultado; 
 }       
 
